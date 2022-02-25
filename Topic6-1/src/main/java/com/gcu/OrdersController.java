7280cddec00256fc476fa4fe234a7210560595df
WarrenGSlice <WarrenPeterson141414@Gmail.com>
2022-02-25 01:50:41 -0800

    Topic 6-1 CST-339

---
+++ Topic6-1/src/main/java/com/gcu/OrdersController.java
@@ -0,0 +1,32 @@
+package com.gcu;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gcu.business.OrdersBusinessService;
+import com.gcu.model.OrderModel;
+
+@Controller
+@RequestMapping("/orders")
+public class OrdersController 
+{
+	@Autowired
+	private OrdersBusinessService service;
+	
+	@GetMapping("/display")
+	public String display(Model model)
+	{
+		// Get some Orders
+		List<OrderModel> orders = service.getOrders();
+		
+		// Display the Orders View
+		model.addAttribute("title", "My Orders");
+		model.addAttribute("orders", orders);
+		return "orders";
+	}
+}
---
+++ Topic6-1/src/main/java/com/gcu/entity/OrderEntity.java
@@ -0,0 +1,82 @@
+package com.gcu.entity;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection="orders")
+public class OrderEntity {
+
+	@Id
+	String id;
+	
+	@Indexed(unique=true)
+	String orderNo;
+	
+	@Indexed(unique=true)
+	String productName;
+	
+	float price;
+	int quantity;
+
+	
+	
+	
+	public OrderEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public OrderEntity(String id, String orderNo, String productName, float price, int quantity) {
+		super();
+		this.id = id;
+		this.orderNo = orderNo;
+		this.productName = productName;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getOrderNo() {
+		return orderNo;
+	}
+
+	public String setOrderNo() {
+		return orderNo;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
