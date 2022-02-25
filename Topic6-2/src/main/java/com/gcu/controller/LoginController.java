a6b37d1b3b7ea336c05fc288a9f2760d879bdd48
WarrenGSlice <WarrenPeterson141414@Gmail.com>
2022-02-25 01:51:20 -0800

    Topic 6-2 CST-339 2/25/2022 by Warren Peterson

---
+++ Topic6-2/src/main/java/com/gcu/controller/LoginController.java
@@ -0,0 +1,43 @@
+package com.gcu.controller;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String display(Model model) {
+		// Display the Login Form
+		model.addAttribute("title", "Login Form");
+		return "login";
+	}
+	
+	//String encoded = new BCryptPasswordEncoder().encode("test");
+	
+
+//	@PostMapping("/doLogin")
+//	public String doLogin(@Valid LoginModel loginModel, BindingResult bindingResult, Model model) {
+//
+//		service.test();
+//		security.authenticate("username","password");
+//
+//		// Check for validation errors
+//		if (bindingResult.hasErrors()) {
+//			model.addAttribute("title", "Login Form");
+//			return "login";
+//		}
+//
+//
+//		service.getOrders();
+//		
+//		
+//
+//		// Display the Orders View
+//		model.addAttribute("title", "My Orders");
+//		model.addAttribute("orders", service.getOrders());
+//		return "orders";
+//	}
+}
